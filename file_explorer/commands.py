7e25ace71426d7c0e0e6829f6157e0251b04ad13
AnikethDandu <aniketh.dandu@icloud.com>
2020-10-08 20:05:56 -0400

    Add ability to move through multiple directories

--- file_explorer/commands.py
+++ file_explorer/commands.py
@@ -1,6 +1,21 @@
+import os
+
+
 def cancel(self):
     return lambda: self.master.destroy()
 
 
-def open_directory(path):
-    return open(file=path, mode='r') if path[-3:] == '.txt' else print(f'Open directory: {path}')
+def return_directories(path):
+    return list(directory for directory in os.listdir(path))
+
+
+def open_directory(directory, master):
+    master.starting_path = master.starting_path.joinpath(directory)
+    return open(file=directory, mode='r') \
+        if directory[-3:] == '.txt' else set_list(master, return_directories(path=master.starting_path))
+
+
+def set_list(master, value,):
+    master.destroy_directory_widgets()
+    master.current_directories = value
+    master.show_path()
--- file_explorer/main.py
+++ file_explorer/main.py
@@ -1,15 +1,16 @@
+import pathlib
 import tkinter
 import tkinter.ttk as ttk
 import file_explorer.commands as commands
-import os
-import pathlib
 
 
 class Application(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.selected_path = tkinter.IntVar()
-        self.current_directories = list(path for path in os.listdir(pathlib.Path.home()))
+        self.current_directory_widgets = []
+        self.starting_path = pathlib.Path.home()
+        self.current_directories = commands.return_directories(self.starting_path)
 
         self.master.rowconfigure(2, weight=1)
 
@@ -30,10 +31,15 @@ class Application(ttk.Frame):
             directory_button = tkinter.Radiobutton(self.main_frame, text=directory, indicatoron=0,
                                                    value=directory_index, variable=self.selected_path)
             directory_button.grid(column=1, row=directory_index, columnspan=3, sticky=tkinter.W)
+            self.current_directory_widgets.append(directory_button)
 
     def assign_commands(self):
         self.cancel_button['command'] = commands.cancel(self=self)
-        self.open_button['command'] = lambda: commands.open_directory(self.current_directories[self.selected_path.get()])
+        self.open_button['command'] = lambda: commands.open_directory(self.current_directories[self.selected_path.get()]
+                                                                      , self)
+
+    def destroy_directory_widgets(self):
+        [button.destroy() for button in self.current_directory_widgets]
 
 
 root = tkinter.Tk()

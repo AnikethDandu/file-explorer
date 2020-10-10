fcd9da17507704afc1bb22443405091fadec6576
AnikethDandu <aniketh.dandu@icloud.com>
2020-10-10 01:05:58 -0400

    Document code

--- file_explorer/commands.py
+++ file_explorer/commands.py
@@ -1,23 +1,81 @@
+"""
+This module has methods for widget commands in the Application class located in main.py
+
+METHODS:
+    cancel
+        Calls inherited destroy method
+    return_directories
+        Returns all directories at specified path
+    open_directory
+        Returns lambda that opens text file or re-initializes current path
+    set_list
+        Re-initializes list of directories at path
+"""
+
 import os
 import pathlib
 from typing import Any
 
 
-def cancel(self):
-    return lambda: self.master.destroy()
+def cancel(application):
+    """
+    Calls the inherited destroy method from the parent class of parameter class
+
+    :param application: class that calls method
+    :type application: Application
+
+    :return: lambda object that contains destroy method
+    :rtype: lambda
+    """
+
+    return lambda: application.master.destroy()
 
 
 def return_directories(path: pathlib.Path) -> list:
+    """
+    Returns a list of directories at a given path
+
+    :param path: path to list directories at
+    :type path: pathlib.Path
+
+    :return: list of directories
+    :rtype: list of str
+    """
+
     return list(directory for directory in os.listdir(path))
 
 
-def open_directory(directory: str, master) -> Any:
-    master.starting_path = master.starting_path.joinpath(directory)
+def open_directory(directory: str, application) -> Any:
+    """
+    Returns lambda object that opens text file or re-initializes current path and returns directories at new path
+
+    :param directory: name of directory that was opened
+    :type directory: str
+    :param application: class whose widget called open_directory
+    :type application: Application
+
+    :return: lambda object to open text file or directory
+    :rtype: lambda
+    """
+
+    application.starting_path = application.starting_path.joinpath(directory)
     return open(file=directory, mode='r') \
-        if directory[-3:] == '.txt' else set_list(master, return_directories(path=master.starting_path))
+        if directory[-3:] == '.txt' else set_list(application, return_directories(path=application.starting_path))
 
 
-def set_list(master, value: list) -> None:
-    master.destroy_directory_widgets()
-    master.current_directories = value
-    master.show_path()
+def set_list(application, directories: list) -> None:
+    """
+    Clears Frame of widgets, re-initializes list of directories. and repopulates Frame with new widgets
+
+    :param application: class whose widget called set_list
+    :type application: Application
+    :param directories: list of directories to re-initialize current_directories to
+    :type directories: list of str
+
+    :return: None
+    :rtype: None
+    """
+
+    application.destroy_directory_widgets()
+    application.current_directories = directories
+    application.show_path()
--- file_explorer/main.py
+++ file_explorer/main.py
@@ -1,3 +1,14 @@
+"""
+This module creates and populates a TkInter frame with the necessary GUI widgets
+
+CLASSES:
+    Application
+
+OBJECTS:
+    root
+    application
+"""
+
 import pathlib
 import tkinter
 import tkinter.ttk as ttk
@@ -5,7 +16,39 @@ import file_explorer.commands as commands
 
 
 class Application(ttk.Frame):
+    """
+    Creates a ttk Frame widget and populates it with navigation buttons and a list of directories based on current path
+
+    METHODS:
+        show_path
+        assign_commands
+        destroy_directory_widgets
+
+    INSTANCE ATTRIBUTES:
+        :ivar selected_path: index of selected directory RadioButton
+        :type selected_path: tkinter.IntVar()
+        :ivar current_directory_widgets: list of all RadioButton widgets corresponding to each directory at current path
+        :type current_directory_widgets: list of tkinter.Button
+        :ivar starting_path: root path
+        :type starting_path: pathlib.Path
+        :ivar current_directories: list of all current directories available at current path
+        :type current_directories: list of str
+        :ivar main_frame: frame that is master widget for all other widgets
+        :type main_frame: ttk.Frame
+        :ivar open_button: button to open directory or file
+        :type open_button: ttk.Button
+        :ivar close_button: button to close application
+        :type close_button: ttk.Button
+    """
+
     def __init__(self, master=None):
+        """
+        Creates an application class inheriting from the tkinter.Tk class, master frame widget, and navigation buttons
+
+        :param master: root class to inherit from
+        :type master: tkinter.Tk
+        """
+
         super().__init__(master)
         self.selected_path = tkinter.IntVar()
         self.current_directory_widgets = []
@@ -20,12 +63,18 @@ class Application(ttk.Frame):
         self.open_button = ttk.Button(self.main_frame, text='Open')
         self.open_button.grid(column=3, row=3)
 
-        self.cancel_button = ttk.Button(self.main_frame, text='Close')
-        self.cancel_button.grid(column=3, row=2, padx=(2.5, 5))
+        self.close_button = ttk.Button(self.main_frame, text='Close')
+        self.close_button.grid(column=3, row=2, padx=(2.5, 5))
 
         self.assign_commands()
 
     def show_path(self) -> None:
+        """
+        Displays every directory at current path as RadioButton
+
+        :return: None
+        :rtype: None
+        """
         count = 0
         for directory in self.current_directories:
             if directory[0:1] != '.':
@@ -37,11 +86,23 @@ class Application(ttk.Frame):
                 self.current_directory_widgets.append(directory_button)
 
     def assign_commands(self) -> None:
-        self.cancel_button['command'] = commands.cancel(self=self)
+        """
+        Sets commands for navigation buttons
+
+        :return:None
+        :rtype: None
+        """
+        self.close_button['command'] = commands.cancel(application=self)
         self.open_button['command'] = lambda: commands.open_directory(self.current_directories[self.selected_path.get()]
                                                                       , self)
 
     def destroy_directory_widgets(self) -> None:
+        """
+        Destroys all directory RadioButton widgets
+
+        :return: None
+        :rtype: None
+        """
         [button.destroy() for button in self.current_directory_widgets]
 
 
